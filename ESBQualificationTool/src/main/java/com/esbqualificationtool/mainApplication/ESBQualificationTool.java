fe4feb88635e4b1f592ceed883c8d4cdf1c43e95
Loic Boyeldieu <loic.boyeldieu@gmail.com>
2016-01-09 18:54:21 +0100

    MVC has been set up

---
+++ ESBQualificationTool/src/main/java/com/esbqualificationtool/mainApplication/ESBQualificationTool.java
@@ -0,0 +1,9 @@
+package com.esbqualificationtool.mainApplication;
+
+public class ESBQualificationTool {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
---
+++ ESBQualificationTool/src/main/java/com/esbqualificationtool/view/ESBQualificationToolWindow.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.esbqualificationtool.view;
+
+/**
+ *
+ * @author root
+ */
+public class ESBQualificationToolWindow {
+
+}
